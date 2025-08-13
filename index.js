bd5b9e92d93ff638d604c0e8732661857af4591e
Mahendhrareddy <mahireddy7569@gmail.com>
2025-08-14 00:49:06 +0530

    cart

--- index.js
+++ index.js
@@ -68,8 +68,10 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://e-commerce-test-drkl.vercel.app" || "http://localhost:5173",
+      "https://nxttrendprojectmahi-euihkib5e.vercel.app",
+      "http://localhost:5173",
     ],
+
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
