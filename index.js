7d117f969be69e137d7c0e4b0151dac21056ed7f
Mahendhrareddy <mahireddy7569@gmail.com>
2025-08-16 12:32:48 +0530

    added

--- index.js
+++ index.js
@@ -227,8 +227,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://nxttrendprojectmahi-euihkib5e.vercel.app",
-      "http://localhost:3000",
+      "http://localhost:5173", // Vite local
+      "http://localhost:3000", // CRA local
+      "https://nxttrendprojectmahi-euihkib5e.vercel.app", // deployed frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
